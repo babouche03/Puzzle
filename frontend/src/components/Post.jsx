3bed05d927db5eacf6c0ef4b2fa3f22b7c1704ca
Babouche03 <2433639762@qq.com>
2024-08-09 01:07:03 +0800

    8.8

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,4 +1,3 @@
-import { Link } from '@chakra-ui/react'
 import {Avatar} from '@chakra-ui/react'
 import {Box, Flex, Image, Text} from '@chakra-ui/react'
 import { BsThreeDots } from "react-icons/bs";
@@ -6,11 +5,13 @@ import Actions from './Actions'
 import { useState, useEffect } from 'react'; 
 import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 import useShowToast from "../hooks/useShowToast";
+import { Link,useNavigate } from'react-router-dom';
+import {formatDistanceToNow} from "date-fns";
 
 const Post = ({post,postedBy}) => {
-    const [liked,setLiked] = useState(false);
     const showToast = useShowToast();
     const [user, setUser] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
 		const getUser = async () => {
@@ -47,13 +48,19 @@ const Post = ({post,postedBy}) => {
         });
     };
   return (
-   <Link to={"markzuckerberg/post/1"}>
+   <Link to={`/${user.username}/post/${post._id}`}>
    <Flex gap={3} mb={4} py={5}>
    <Flex flexDirection={"column"} alignItems={"center"}>
-       <Avatar size='md' name={user.name} src={user?.profilePic} />
+       <Avatar size='md' name={user.name} src={user?.profilePic}
+         onClick={(e)=>{
+            e.preventDefault();
+            navigate(`/${user.username}`);
+         }}
+       
+       />
        <Box w='1px' h={"full"} bg='gray.light' my={2}></Box>
        <Box position={"relative"} w={"full"}>
-        {post.replies.length === 0 && <Text textAlign={"center"}></Text>}
+        {post.replies.length === 0 && <Text textAlign={"center"}>👀</Text>}
         {post.replies[0] &&(
           <Avatar 
                size='xs'
@@ -95,14 +102,19 @@ const Post = ({post,postedBy}) => {
    <Flex flex={1} flexDirection={"column"} gap={2}>
        <Flex justifyContent={"space-between"} w={"full"}>
            <Flex w={"full"} alignItems={"center"}>
-               <Text fontSize={"sm"} fontWeight={"bold"}>
+               <Text fontSize={"sm"} fontWeight={"bold"}
+                 onClick={(e)=>{
+                    e.preventDefault();
+                    navigate(`/${user.username}`);
+                 }}
+               >
                    {user?.username}
                </Text>
                <Image src='/verified.png' w={4} h={4} ml={1} />
            </Flex>
            <Flex gap={4} alignItems={"center"}>
-               <Text fontStyle={"sm"} color={"gray.light"}>
-                   1d
+               <Text fontSize={"sm"} width={36} textAlign={"right"} color={"gray.light"}>
+                   {formatDistanceToNow(new Date(post.createdAt))} ago
                </Text>
                <Menu>
     <MenuButton>
@@ -125,20 +137,10 @@ const Post = ({post,postedBy}) => {
        )}
        
           
-    
        <Flex gap={3} my={1}>
-           <Actions liked={liked} setLiked={setLiked}/>
+           <Actions post={post} />
        </Flex>
 
-       <Flex gap={2} alignItems={"center"}>
-           <Text color={"gray.light"} fontSize='sm'>
-               {post.replies.length} replies
-           </Text>
-           <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
-           <Text color={"gray.light"} fontSize='sm'>
-               {post.likes.length} likes
-           </Text>
-       </Flex>
    </Flex>
 </Flex>
    </Link>
--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -5,37 +5,58 @@ import useShowToast from "../hooks/useShowToast";
 import { Flex, Spinner } from "@chakra-ui/react";
 import { useRecoilState } from "recoil";
 import UserPost from "../components/UserPost";
+import Post from "../components/Post";
 
 const UserPage = () => {
   const [user,setUser] = useState(null);
   const { username } =useParams();
   const showToast = useShowToast();
   const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+	const [fetchingPosts, setFetchingPosts] = useState(true);
 
 	useEffect(() => {
 		const getUser = async () => {
-		
 			try {
 				const res = await fetch(`/api/users/profile/${username}`);
 				const data = await res.json();
 				if(data.error) {
-          showToast("Error", data.error, "error");
-          return;
-        }
-        setUser(data);
+					showToast("Error", data.error, "error");
+					return;
+				}
+                setUser(data);
+				} catch (error) {
+					showToast("Error", error.message, "error");
+				} finally {
+			setLoading(false);
+         }
+	};
+
+    const getPosts = async () => {
+			if (!user) return;
+			setFetchingPosts(true);
+			try {
+				const res = await fetch(`/api/posts/user/${username}`);
+				const data = await res.json();
+				console.log(data);
+				setPosts(data);
 			} catch (error) {
 				showToast("Error", error.message, "error");
+				setPosts([]);
 			} finally {
-        setLoading(false);
-      }
+				setFetchingPosts(false);
+			}
 		};
 
 		getUser();
+    getPosts();
 	}, [username, showToast]);
 
   if(!user && loading) {
     return (
-      <Spinner size="xl" />
+      <Flex justifyContent={"center"}>
+      <Spinner size={"xl"} />
+      </Flex>
     )
   }
 
@@ -44,13 +65,62 @@ const UserPage = () => {
   return (
     <> 
      <UserHeader user={user} />
-     <UserPost likes={1000} replies={100} postImg="/post1.png" postTitle="This is the first post."/>
-     <UserPost likes={231} replies={17} postImg="/post2.png" postTitle="This is the 2nd post."/>
-     <UserPost likes={1000} replies={100} postImg="/post3.png" postTitle="This is the 3rd post."/>
-     <UserPost likes={120} replies={200} postTitle="This is the 4rd post."/>
+
+     {!fetchingPosts && posts.length === 0 && <h1>User has not posts.</h1>}
+			{fetchingPosts && (
+				<Flex justifyContent={"center"} my={12}>
+					<Spinner size={"xl"} />
+				</Flex>
+			)}
+
+			{posts.map((post) => (
+				<Post key={post._id} post={post} postedBy={post.postedBy} />
+			))}
     </>
 
   )
 }
 
 export default UserPage;
+
+
+//useEffect(() => {
+	// const getUser = async () => {
+	// 	try {
+	// 	  const res = await fetch(`/api/users/profile/${username}`);
+	// 	  const data = await res.json();
+	// 	  if (data.error) {
+	// 		showToast("Error", data.error, "error");
+	// 		return;
+	// 	  }
+	// 	  setUser(data);
+	// 	} catch (error) {
+	// 	  showToast("Error", error.message, "error");
+	// 	} finally {
+	// 	  setLoading(false);
+	// 	}
+	//   };
+	
+	//   getUser();
+	// }, [username, showToast]);
+	
+	// useEffect(() => {
+	//   const getPosts = async () => {
+	// 	if (!user) return;
+	// 	setFetchingPosts(true);
+	// 	try {
+	// 	  const res = await fetch(`/api/posts/user/${username}`);
+	// 	  const data = await res.json();
+	// 	  setPosts(Array.isArray(data) ? data : []);
+	// 	} catch (error) {
+	// 	  showToast("Error", error.message, "error");
+	// 	  setPosts([]);
+	// 	} finally {
+	// 	  setFetchingPosts(false);
+	// 	}
+	//   };
+	
+	//   if (user) {
+	// 	getPosts();
+	//   }
+	// }, [user, username, showToast]);
